92e0f4eb5693fd52fa4cafb13d654cdcacc9cf57
Tyler-D <tylerzy@foxmail.com>
2017-04-24 15:36:24 +0800

    add shuffle

--- ssd_tools/create_list.py
+++ ssd_tools/create_list.py
@@ -2,14 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import os
+import random
 
-IMG_ROOT_PATH="/home/caffemaker/caffe/dataset/pic_video/img"
-IMG_LIST="/home/caffemaker/caffe/dataset/pic_video/img_list"
+FILE_LIST_PATH = "/home/caffemaker/ZTE/img_list"
+TRAIN_FILE_PATH = "/home/caffemaker/ZTE/train.txt"
+VAL_FILE_PATH = "/home/caffemaker/ZTE/val.txt"
+train_list = open(TRAIN_FILE_PATH,"w")
+val_list = open(VAL_FILE_PATH,"w")
+file = open(FILE_LIST_PATH,"r")
 
-img_list = open(IMG_LIST,"w")
-for root, dirs, files in os.walk(IMG_ROOT_PATH):
-    for img in files:
-        img_path = os.path.join(root,img)
-        list_info = img_path + "\n"
-        img_list.write(list_info)
+file_list = file.readlines()
+
+random.shuffle(file_list)
+
+val_num = int(len(file_list)*0.1)
+cnt = 0
+for pair in file_list:
+    if (cnt < val_num):
+        info_list = val_list
+    else :
+        info_list = train_list
+    info_str = pair
+    info_list.write(info_str)
+    cnt+=1
+
 
